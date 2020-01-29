b3fc90f3225daa78f6fb08611298fd495dc1f7e0
nadiaaoliverr <nasilvah21@gmail.com>
2020-01-28 22:47:25 -0300

    Adicionando exemplo de números com casas decimais

---
+++ numeros_demais.py
@@ -0,0 +1,17 @@
+#https://www.alura.com.br/artigos/precisao-numeros-decimais-python
+
+#Números sem precisão
+ganhos_julho = 99.91 * 5
+print(ganhos_julho)
+
+gastos_julho = 110.1 * 3
+print(gastos_julho)
+
+#Números com precisão
+from decimal import Decimal
+
+ganhos_julho = Decimal('99.91') * 5
+print(ganhos_julho)
+
+gastos_julho = Decimal('110.1') * 3
+print(gastos_julho)
