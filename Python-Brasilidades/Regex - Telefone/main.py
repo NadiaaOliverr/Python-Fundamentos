a633c47aca5ec9127bee11534a567718b5c30242
nadiaaoliverr <nasilvah21@gmail.com>
2020-01-31 10:28:51 -0300

    Mudança do padrão da regex de telefone

--- Python-Brasilidades/Regex - Telefone/main.py
+++ Python-Brasilidades/Regex - Telefone/main.py
@@ -1,5 +1,6 @@
 from telefones_br import telefones_br
+import re
 
-telefone = "552126481234"
+telefone = "55(21)981524578"
 telefone_objeto = telefones_br(telefone)
 print(telefone_objeto)
--- Python-Brasilidades/Regex - Telefone/telefones_br.py
+++ Python-Brasilidades/Regex - Telefone/telefones_br.py
@@ -11,7 +11,7 @@ class telefones_br:
         return self.format_numero()
             
     def valida_telefone(self, telefone):
-        padrao = "([0-9]{2,3})?([0-9]{2})([0-9]{4,5})([0-9]{4})"
+        padrao = "([0-9]{2,3})?(\([0-9]{2}\))([0-9]{4,5})([0-9]{4})"
         resposta = re.findall(padrao, telefone)
         if resposta:
             return True
@@ -19,9 +19,9 @@ class telefones_br:
             return False
     
     def format_numero(self):
-        padrao = "([0-9]{2,3})?([0-9]{2})([0-9]{4,5})([0-9]{4})"
+        padrao = "([0-9]{2,3})?(\([0-9]{2}\))([0-9]{4,5})([0-9]{4})"
         resposta = re.search(padrao, self.numero)
-        numero_formatado = "+{}({}){}-{}".format(
+        numero_formatado = "+{}{}{}-{}".format(
             resposta.group(1),resposta.group(2),resposta.group(3),
             resposta.group(4)
         )
