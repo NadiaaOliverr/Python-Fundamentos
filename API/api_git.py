944d95828388604364d97b87a13c77794e0f0098
nadiaaoliverr <nasilvah21@gmail.com>
2020-02-02 17:06:34 -0300

    Organizando arquivos

--- API/api_git.py
+++
@@ -1,8 +0,0 @@
-import requests, json
-
-response = requests.get("https://api.github.com/users/nadiaaoliverr")
-json_response = response.json()
-
-formatted_response = json.dumps(json_response, indent=2,ensure_ascii=False)
-
-print(formatted_response))
--- Datas/main.py
+++
@@ -1,7 +0,0 @@
-from datas import DatasBr
-
-cadastro = DatasBr()
-print(cadastro.tempo_cadastro())
-
-# print(datetime.today())
-# datetime.timedelta
