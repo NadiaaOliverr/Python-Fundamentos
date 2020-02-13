66037a436b2825f3b655b414cc1258324ae998d8
Nádia Oliveira <nadiaaoliverr@gmail.com>
2020-02-13 16:28:52 -0300

    Adicionando projeto base para testes

---
+++ Testes/src/leilao/dominio.py
@@ -0,0 +1,26 @@
+class Usuario:
+
+    def __init__(self, nome):
+        self.__nome = nome
+
+    @property
+    def nome(self):
+        return self.__nome
+
+
+class Lance:
+
+    def __init__(self, usuario, valor):
+        self.usuario = usuario
+        self.valor = valor
+
+
+class Leilao:
+
+    def __init__(self, descricao):
+        self.descricao = descricao
+        self.__lances = []
+
+    @property
+    def lances(self):
+        return self.__lances
